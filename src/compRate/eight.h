833c8f81e66b68eb4922f519d8a6ea8423f92e1f
jgeb28 <jan-geb28@wp.pl>
2023-03-27 20:42:58 +0200

    dictionary decompressing function 8bit version

--- src/compRate/eight.h
+++ src/compRate/eight.h
@@ -2,11 +2,14 @@
 #define EIGHT_H
 
 #include <stdio.h>
+#include "node.h"
 
 int eightAnalyzeInput( FILE* in, int* charcounter, int uniquecounter );
 
 void eightOutputGenerator( FILE* in, int uniqueCounter, unsigned short** codes, FILE *out, char password, char *remainingChar, int remainingLen );
 
 int eightDictionary( node *pointer, node *border, node *root, node *last, FILE *out, int buforLength, char *bufor);
+
+node *readDicEight(FILE *in, char *bufor, int buforLength, unsigned short *charzero, unsigned short *character, int charlen);
 
 #endif
