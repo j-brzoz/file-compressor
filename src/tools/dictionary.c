3f71cceb34ee5df097fb55cf8049751a840ff7c2
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2023-04-01 22:59:00 +0200

    new dictionary

---
+++ src/tools/dictionary.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "./convert.h"
+
+int dictionary(unsigned short **codes, FILE *out, int uniqueCounter, int inputSize, char *bufor){
+    unsigned char byte[1];
+
+    // dictionary length in bits
+    unsigned int dictLength = 0;
+    for(int i = 0; i < uniqueCounter; i++){
+        //           charcter  codeLen code
+        dictLength += inputSize + 8 + codes[i][1];
+    }
+    
+    byte[0] = (dictLength >> 24) & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    byte[0] = (dictLength >> 16) & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    byte[0] = (dictLength >> 8) & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    byte[0] = dictLength & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    // unique counter
+    byte[0] = (uniqueCounter >> 24) & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    byte[0] = (uniqueCounter >> 16) & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    byte[0] = (uniqueCounter >> 8) & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    byte[0] = uniqueCounter & 0xFF;
+    fwrite(byte, 1, 1, out);
+
+    int buforLength = 0;
+    char *value;
+    char *byteBin = malloc(8 * sizeof * byteBin);
+
+    for(int i = 0; i < uniqueCounter; i++){
+        
+        value = DectoBin(codes[i][0], inputSize);
+        for(int j = 0; j < inputSize; j++){
+            bufor[j+buforLength] = value[j];
+        }
+        buforLength += inputSize;
+
+        value = DectoBin(codes[i][1], 8);
+        for(int j = 0; j < 8; j++){
+            bufor[j+buforLength] = value[j];
+        }
+        buforLength += 8;
+        
+        for(int j = 0; j < codes[i][1]; j++){
+            bufor[j+buforLength] = codes[i][j+2];
+        }
+        buforLength += codes[i][1];
+
+        while( buforLength >= 8 ) {
+        
+            // get code from bufor
+            for(int j = 0; j < 8; j++) {
+                byteBin[j] = bufor[j];
+            }
+            
+            // convert code to char
+            byte[0] = binToDec( byteBin );
+            
+            // write character
+            fwrite( byte, 1, 1, out );
+            
+            // move codes in bufor
+            for( int j = 0; j < buforLength - 8; j++ ) {
+                bufor[j] = bufor[j + 8];
+            }
+            buforLength -= 8;
+        }
+    }
+
+    free(byteBin);
+    free(value);
+
+    return buforLength;
+}
---
+++ src/tools/dictionary.h
@@ -0,0 +1,8 @@
+#ifndef DICT_H
+#define DICT_H
+
+#include <stdio.h>
+
+int dictionary(unsigned short **codes, FILE *out, int uniqueCounter, int inputSize, char *bufor);
+
+#endif
